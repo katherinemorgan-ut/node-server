bdc8ad371a89870b580f167af89a7c6749ba8e86
katherinemorgan-ut <katherinemorgan.ut@gmail.com>
2025-10-01 16:32:57 -0600

    discussing middleware

--- app.js
+++ app.js
@@ -1,59 +1,29 @@
 const express = require('express');
+const logger = require('./logger.js');
+const authorize = require('./authorize.js');
 const app = express();
 
-const products = require('./products.json');
+// req => middleware => res
+// app.use([logger, authorize]);
+
+app.use('/api', logger)
+// will match any path with /api, e.g. api/home/about/product will still apply
 
 app.get('/', (req, res) => {
-    res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+    res.send('Home')
 })
 
-app.get('/api/products', (req,res) => {
-    const newProducts = products.map((product) => {
-        const {id, name} = product;
-        return {id, name}
-    })
-    res.json(newProducts)
+app.get('/about', (req, res) => {
+    res.send('About Page')
 })
 
-app.get('/api/products/:productID', (req,res) => {
-    // console.log(req);
-    // console.log(req.params);
-    const {productID} = req.params;
-
-    // Req params is always a string, so we must cast it as a number
-    const singleProduct = products.find((product) => product.id == Number(productID))
-    if(!singleProduct) {
-        return res.status(404).send('Product does not exist')
-    }
-    
-    return res.json(singleProduct)
+app.get('/product', (req, res) => {
+    res.send('Products')
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    console.log(req.params);
-    res.send('hello world')
-})
-
-
-app.get('/api/v1/query', (req, res) => {
-    // console.log(req.query);
-    const {search,limit} = req.query;
-    let sortedProducts = [...products];
-
-    if(search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit) {
-        sortedProducts = sortedProducts.slice(0,Number(limit));
-    }
-    if(sortedProducts.length < 1) {
-        // res.status(200).send('No products match your filter.');
-        return res.status(200).json({success:true, data:[]})
-    }
-
-    return res.status(200).json(sortedProducts);
+app.get('/items', [logger, authorize], (req, res) => {
+    console.log(req.user);
+    res.send('Items')
 })
 
 app.listen(5000, (req,res) => {
